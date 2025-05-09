e25bf238f13ffd73aae38f506883e6dcaba1824e
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2025-05-09 22:20:44 +0200

    initial ui templates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
+import { lazy } from "react";
+import { ThemeProvider } from "./components/theme-provider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/Login";
+const Home = lazy(() => import("./pages/Home"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const AuthenticationPage = lazy(() => import("./pages/AuthenticationPage"));
+
 export default function App() {
   return (
-    <div className="min-h-screen bg-amber-50 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello, Tailwind + Bun!
-      </h1>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<AuthenticationPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
---
+++ frontend/src/components/ui/icons.tsx
@@ -0,0 +1,7 @@
+import { Loader2, Github, AlertCircle } from "lucide-react";
+
+export const Icons = {
+  spinner: Loader2,
+  gitHub: Github,
+  alertCircle: AlertCircle,
+};
